077ca8c3bf1704c90fa8595849e4a2c836c2fd4c
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-24 14:09:02 -0400

    Corregir Theme.kt: agregar import de Color

--- app/src/main/java/org/luisito/admin360/ui/theme/Theme.kt
+++ app/src/main/java/org/luisito/admin360/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package org.luisito.admin360.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val AdminColorScheme = lightColorScheme(
     primary = Color(0xFF4285F4),
