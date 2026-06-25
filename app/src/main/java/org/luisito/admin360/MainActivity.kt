fae11f80dfa0026059e8fd361a59d1129817b52b
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-25 04:22:52 -0400

    Agregar import fillMaxWidth en MainActivity

--- app/src/main/java/org/luisito/admin360/MainActivity.kt
+++ app/src/main/java/org/luisito/admin360/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -110,7 +111,6 @@ fun LoginScreen() {
             onClick = {
                 if (email.isNotEmpty() && password.isNotEmpty()) {
                     isLoading = true
-                    // TODO: Conectar con Supabase
                     error = "Login temporal (conectar con Supabase)"
                     isLoading = false
                 }
