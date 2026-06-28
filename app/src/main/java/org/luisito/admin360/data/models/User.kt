6505ab827a91e47d7ec225ac335a625ad3e9abe5
luisitoichy89-code <luisitoichy89@gmail.com>
2026-06-28 12:22:00 -0400

    FIX: User.kt con @Contextual para UUID, MainActivity corregido, Theme.kt creado

--- app/src/main/java/org/luisito/admin360/data/models/User.kt
+++ app/src/main/java/org/luisito/admin360/data/models/User.kt
@@ -1,12 +1,13 @@
 package org.luisito.admin360.data.models
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Contextual
 import java.util.UUID
 
 @Serializable
 data class User(
     val id: Int,
-    val auth_id: UUID? = null,
+    @Contextual val auth_id: UUID? = null,
     val cliente_id: Int,
     val username: String,
     val nombre: String? = null,
