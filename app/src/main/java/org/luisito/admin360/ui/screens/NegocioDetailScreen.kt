ede18ca723d5267432f3d68b6a6e0c1813574955
luisitoichy89-code <luisitoichy89@gmail.com>
2026-07-06 20:51:15 -0400

    fix: reescribir NegocioDetailScreen.kt con imports correctos

--- app/src/main/java/org/luisito/admin360/ui/screens/NegocioDetailScreen.kt
+++ app/src/main/java/org/luisito/admin360/ui/screens/NegocioDetailScreen.kt
@@ -1,5 +1,3 @@
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 package org.luisito.admin360.ui.screens
 
 import androidx.compose.foundation.layout.*
@@ -8,6 +6,7 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
