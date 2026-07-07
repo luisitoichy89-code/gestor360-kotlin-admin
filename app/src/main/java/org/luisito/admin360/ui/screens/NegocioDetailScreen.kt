e8beceb12944349d1c5a22849552463ff1540363
luisitoichy89-code <luisitoichy89@gmail.com>
2026-07-06 20:12:31 -0400

    fix: eliminar duplicados en ui/ y agregar import tabIndicatorOffset

--- app/src/main/java/org/luisito/admin360/ui/screens/NegocioDetailScreen.kt
+++ app/src/main/java/org/luisito/admin360/ui/screens/NegocioDetailScreen.kt
@@ -1,3 +1,5 @@
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 package org.luisito.admin360.ui.screens
 
 import androidx.compose.foundation.layout.*
